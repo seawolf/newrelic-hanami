5dba2a8a2f4a08667556ecfe243195b75eea97b9
Ben Arnold <ben@seawolfsanctuary.com>
2022-09-22 10:57:48 +0000

    Fix: use Hanami v2 Action class

--- spec/newrelic-hanami/instrument_spec.rb
+++ spec/newrelic-hanami/instrument_spec.rb
@@ -3,24 +3,42 @@ describe NewRelic::Agent::Instrumentation::Hanami do
     NewRelic::Agent::Instrumentation::ControllerInstrumentation
   end
 
-  before do
-    stub_const 'Web::Controllers::Home::Index', Class.new
-    Web::Controllers::Home::Index.class_eval do
-      include Hanami::Action
+  let(:request_params) { {} }
+  let(:response)       { Web::Controllers::Home::Index.new.call(request_params) }
 
-      def call(params)
-        self.body = 'hello'
+  before do
+    stub_const 'Web::Controllers::Home::Index', Class.new(Hanami::Action)
+    Web::Controllers::Home::Index.class_eval do
+      def handle(_req, resp)
+        resp.body = 'hello'
       end
     end
   end
 
-  it 'perform_action_with_newrelic_trace' do
+  it 'calls perform_action_with_newrelic_trace with amended options' do
     expect_any_instance_of(subject).to receive(
-      :perform_action_with_newrelic_trace)
-    code, headers, body = Web::Controllers::Home::Index.new.call({})
+      :perform_action_with_newrelic_trace).with({
+        category: :controller,
+        name:     "TODO HERE",
+        request:  a_kind_of(Hanami::Action::Request),
+        params:   request_params
+      }).once
 
-    expect(code).to    eq(200)
-    expect(headers).to eq({})
-    expect(body).to    eq(['hello'])
+    response
+  end
+
+  it 'passes-through the response status code' do
+    expect(response.status).to eq(200)
+  end
+
+  it 'passes-through the response headers' do
+    expect(response.headers).to eq({
+      'Content-Length' => response.body.first.length.to_s,
+      'Content-Type'   =>  'application/octet-stream; charset=utf-8'
+    })
+  end
+
+  it 'passes-through the response body' do
+    expect(response.body).to eq(['hello'])
   end
 end
