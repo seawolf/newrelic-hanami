8bf878ab5338984807eb5a2447bab552420a2df3
Ben Arnold <ben@seawolfsanctuary.com>
2022-10-04 17:50:37 +0100

    Add: transaction name correction for Actions

--- lib/newrelic-hanami/instrument.rb
+++ lib/newrelic-hanami/instrument.rb
@@ -30,6 +30,49 @@ module NewRelic
             params: params.to_h
           }
         end
+
+        def create_transaction_options(*)
+          super.tap do |txn_options|
+            txn_options[:transaction_name] = CorrectedTransactionName.from_transaction_name(txn_options[:transaction_name])
+          end
+        end
+
+        module CorrectedTransactionName
+          class << self
+            def from_transaction_name(str)
+              str.to_s.split('/').tap do |segments|
+                segments[-1] = action_name(segments[-1])
+              end.join('/')
+            end
+
+            def action_name(str)
+              str.sub(NAME_REGEX, '').split('::').map { ActiveSupportInflector.underscore(_1) }.join('/')
+            end
+
+            # Copy of Rails' `underscore` method from ActiveSupport::Inflector
+            # https://github.com/rails/rails/blob/v7.0.4/activesupport/lib/active_support/inflector/inflections.rb
+            module ActiveSupportInflector
+              ACRONYMS                  = {}
+              ACRONYM_REGEX             = ACRONYMS.empty? ? /(?=a)b/ : /#{ACRONYMS.values.join("|")}/
+              ACRONYMS_UNDERSCORE_REGEX = /(?:(?<=([A-Za-z\d]))|\b)(#{ACRONYM_REGEX})(?=\b|[^a-z])/
+
+              class << self
+                def underscore(camel_cased_word)
+                  return camel_cased_word unless /[A-Z-]|::/.match?(camel_cased_word)
+
+                  word = camel_cased_word.to_s.gsub('::', '/')
+                  word.gsub!(ACRONYMS_UNDERSCORE_REGEX) { "#{$1 && '_'.freeze }#{$2.downcase}" }
+                  word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
+                  word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+                  word.tr!('-', '_')
+                  word.downcase!
+
+                  word
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
