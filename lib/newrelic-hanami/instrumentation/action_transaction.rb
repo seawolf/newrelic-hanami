4373b101999648b0c2f8cd36d2bea9c286dcaa3c
Ben Arnold <ben@seawolfsanctuary.com>
2022-10-04 17:54:46 +0100

    Add: correction of Action naming

--- lib/newrelic-hanami/instrumentation/action_transaction.rb
+++ lib/newrelic-hanami/instrumentation/action_transaction.rb
@@ -23,12 +23,40 @@ module NewRelic
 
           private
 
+          NAME_REGEX = /Controllers::/.freeze
+
           def _trace_options(request, params)
             {
               category: :controller,
               request: request,
               params: params.to_h
             }
+          end
+
+          def create_transaction_options(*)
+            super.tap do |txn_options|
+              txn_options[:transaction_name] =
+                CorrectedTransactionName.from_class_and_method_name(txn_options[:transaction_name])
+            end
+          end
+
+          # Converts a Transaction name
+          # from the "Controller/{Namespace}::{Action}" format
+          # into the "Controller/{namespace}/{action}" format
+          module CorrectedTransactionName
+            class << self
+              def from_class_and_method_name(str)
+                str.to_s.split('/').tap do |segments|
+                  segments[-1] = method_name(segments[-1])
+                end.join('/')
+              end
+
+              def method_name(str)
+                str.sub(NAME_REGEX, '').split('::').map do |segment|
+                  ::NewRelic::Hanami::ActiveSupportInflector.underscore(segment)
+                end.join('/')
+              end
+            end
           end
         end
       end
